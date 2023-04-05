085e6dc90cbbfdd7d63f7eabfc4d265b800f3ff8
Pedro Gomes <pedrogac@hotmail.com>
2023-04-04 23:16:35 -0300

    add iterator map example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
     let square = |x| x * x;
     println!("5 square is {}", square(5));
+
+    let pairs = vec![(0, 1), (2, 3), (4, 5)];
+    pairs
+        .into_iter()
+        // .map(|t| (t.0 + 1, t.1))
+        .map(|(x, y)| (x + 1, y))
+        .for_each(|t| println!("{:?}", t));
 }
